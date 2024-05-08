7a88e6031cd96c5478be6ad5511f44cf767831ee
petermdtran246 <petermdtran246@gmail.com>
2024-05-08 16:23:57 +0700

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int P, T, R;
+    cout << "Enter P, T, R numbers: ";
+    cin >> P >> T >> R;
+    float SI;
+    SI = P*T*R/100.0;
+    cout << "Simple interes0t is: " << SI;
+    return 0;
+}
